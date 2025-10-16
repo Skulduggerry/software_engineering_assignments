43536de31cb72f3fe3c49e5b83d4ede064df17c8
Felix Kretke <f.kretke@gmail.com>
2025-10-16 15:35:39 +0200

    fixed this shit

--- 00_assignments/task2-4/src/RacingCar.java
+++ 00_assignments/task2-4/src/RacingCar.java
@@ -68,6 +68,65 @@ public class RacingCar {
         return pos;
     }
 
+    public static void main(String[] args) {
+        RacingCar car = new RacingCar("Hamilton", 200);
+        assertTrue(car.getRacer().equals("Hamilton"));
+        assertTrue(car.maxSpeed == 200);
+        assertTrue(car.finish == -1);
 
+        car.setRacer("Bottas");
+        assertTrue(car.getRacer().equals("Bottas"));
+        car.setRacer("");
+        assertTrue(car.getRacer().equals("Bottas"));
 
+        car.setSpeed(10);
+        assertTrue(car.getSpeed() == 10);
+        car.setSpeed(0);
+        assertTrue(car.getSpeed() == 0);
+        car.setSpeed(-10);
+        assertTrue(car.getSpeed() == 0);
+        car.setSpeed(car.maxSpeed);
+        assertTrue(car.getSpeed() == car.maxSpeed);
+        car.setSpeed(car.maxSpeed + 10);
+        assertTrue(car.getSpeed() == car.maxSpeed);
+
+        car.setPosition(10);
+        assertTrue(car.getPosition() == 10);
+        car.setPosition(-10);
+        assertTrue(car.getPosition() == 10);
+        car.setPosition(0);
+        assertTrue(car.getPosition() == 0);
+
+        assertTrue(!car.isFinished());
+        car.finish = 20;
+        car.setPosition(10);
+        assertTrue(!car.isFinished());
+        car.setPosition(20);
+        assertTrue(car.isFinished());
+        car.setPosition(22);
+        assertTrue(car.isFinished());
+
+        car.start(50, 200);
+        assertTrue(car.getPosition() == 0);
+        assertTrue(car.getSpeed() == 50);
+        assertTrue(car.finish == 200);
+
+        car.race(0);
+        assertTrue(car.getSpeed() == 50);
+        assertTrue(car.getPosition() == 50);
+        assertTrue(!car.isFinished());
+
+        car.race(10);
+        assertTrue(car.getSpeed() == 60);
+        assertTrue(car.getPosition() == 110);
+        assertTrue(!car.isFinished());
+
+        car.race(-10);
+        assertTrue(car.getSpeed() == 50);
+        assertTrue(car.getPosition() == 160);
+        assertTrue(!car.isFinished());
+
+        car.race(0);
+        assertTrue(car.isFinished());
+    }
 }
